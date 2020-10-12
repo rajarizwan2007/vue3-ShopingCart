ddeb8ffcf66a5543314b830455cc05c92733f74e
vagrant <vagrant@homestead>
2020-10-12 14:03:46 +0000

    Vue Forms featurs, Submitting a form and showing its value on the same page using v-model

--- components/productDisplay.js
+++ components/productDisplay.js
@@ -51,37 +51,44 @@ app.component('product-display', {
 
       </div>
     </div>
+    <review-list v-if="reviews.length" :reviews="reviews"></review-list>
+    <review-form @review-submitted="addReview"></review-form>
   </div>`,
   data() {
     return {
         product: 'Socks',
-        brand: 'Vue Mastery',
+        brand: 'My Brand',
         selectedVariant: 0,
-        details: ['50% cotton', '30% wool', '20% polyester'],
+        details: ['30% cotton', '40% wool', '30% polyester'],
         variants: [
-          { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
-          { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
-        ]
+          { id: 100, color: 'green', image: './assets/images/socks_green.jpg', quantity: 10 },
+          { id: 200, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 1 },
+        ],
+        reviews: []
     }
   },
   methods: {
       addToCart() {
           this.$emit('add-to-cart', this.variants[this.selectedVariant].id)
       },
-      // solution
+      // Communicating Events
       removeFromCart() {
         this.$emit('remove-from-cart', this.variants[this.selectedVariant].id)
       },
-      // solution
+      // Communicating Events
       updateVariant(index) {
           this.selectedVariant = index
+      },
+      addReview(review) {
+        this.reviews.push(review)
       }
   },
   computed: {
       title() {
-          return this.brand + ' ' + this.product
+          return this.brand + ' - ' + this.product
       },
       image() {
+          /* one way binding */
           return this.variants[this.selectedVariant].image
       },
       inStock() {
@@ -91,7 +98,7 @@ app.component('product-display', {
         if (this.premium) {
           return 'Free'
         }
-        return 2.99
+        return 3.99
       }
   }
 })
